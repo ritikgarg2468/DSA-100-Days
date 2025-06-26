7bd3195dad8dddf744bf0556ba4bf8071f47f981
Ritik Garg <ritikgarg2468@gmail.com>
2025-06-26 11:01:03 +0530

    Day 4

---
+++ Sorting/Sorting.java
@@ -0,0 +1,109 @@
+import java.util.*;
+
+public class Sorting{
+    public static void bubbleSort(int arr[]){
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = 0; j < arr.length-1-i; j++) {
+                if (arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+                
+            }
+            
+        }
+
+    }
+
+    public static void selectionSort(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int min_idx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min_idx]) {
+                    min_idx = j;
+                }
+            }
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void insertionSort(int arr[]) {
+        int n = arr.length;
+        for (int i = 1; i < n; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void quickSort(int arr[], int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    private static int partition(int arr[], int low, int high) {
+        int pivot = arr[high];
+        int i = (low - 1);
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+
+        return i + 1;
+    }
+
+    public static void printArr(int arr[]){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+    
+    public static void printArr1(Integer arr[]){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {3,5,4,1,2}; 
+        // bubbleSort(arr);
+        // quickSort(arr, 0, 0);
+        // selectionSort(arr);
+        // insertionSort(arr);
+
+        // inbuild sorting in java-------------------------------------------
+        // Arrays.sort(arr);
+        // Arrays.sort(arr,0,3);
+          // for collection use object Integer not int---------
+          // Integer arr1[] = {3,5,4,1,2};
+          // Arrays.sort(arr1, Collections.reverseOrder());
+          // Arrays.sort(arr1,0,3, Collections.reverseOrder());
+          // printArr1(arr1);
+          //---------------------------------------------------
+        //-------------------------------------------------------------------
+
+        printArr(arr);
+        
+    }
+}
